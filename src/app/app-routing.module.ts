389a248e27ae842d29c9c218e013a7c9866a56e7
Maciej Paluch <maciekpal@gmail.com>
2022-11-15 18:11:44 +0100

    Exam - Details
    Product list path : "/products
    Product details with image path : "/products/:id"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductsListComponentModule } from './components/products-list/products-list.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
+import { ProductDetailsComponentModule } from './components/product-details/product-details.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{ path: 'products', component: ProductsListComponent }, { path: 'products/:id', component: ProductDetailsComponent }]), ProductsListComponentModule, ProductsServiceModule, ProductDetailsComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
